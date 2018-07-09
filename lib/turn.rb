3c16b0fedf3346a4fb0087bc1a4d16311d87cff6
Anita Beauchamp <amadamspr@gmail.com>
2018-07-09 02:37:24 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,15 @@
+# display_board displays the Tic Tac Toe board
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# input_to_index converts user input to array index value
+def input_to_index(input)
+  input.to_i - 1
 end
 
 def turn(board)
