a5093c6906af4d0e76b60fc73551c2351c0d5098
Anita Beauchamp <amadamspr@gmail.com>
2018-07-09 02:40:00 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,6 +12,26 @@ def input_to_index(input)
   input.to_i - 1
 end
 
+# code your #valid_move? method here
+def valid_move?(board, index)
+  if index.between?(0,8)
+    !position_taken?(board, index)
+  else
+    false
+  end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken? (board, index)
+  if board[index].nil? || board[index] == "" || board[index] == " "
+    false
+  else
+    true
+  end
+end
+
+
 def turn(board)
   puts "Please enter 1-9:"
 end
+
